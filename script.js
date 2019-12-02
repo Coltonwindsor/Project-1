a2822f1b555fd5d99c3453fc3ec211b4ca75da1b
Colton Windsor <Coltonwindsor@yahoo.com>
2019-12-02 15:11:20 -0500

    pseudocode

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+// jeoprady blue hex - #060CE9
 console.log('this is a test')
+
+// Pseudocode
+
+// Create a heading - This... Is Jeoprady   or just use the Jeoprady logo
+
+// Create a paragraph containing the instructions for the game.
+
+// Create a play button that takes the user to the game board when clicked.
+
+// Bonus - create an input field for the user to type in their name to displayed under the game board.
+
+// Using javascript - create the game board (6x6) with categories listed on the top row and point values displayed in the squares underneath.
+
+// Create a global variable to track the players score, set it equal to zero. Points will be added to or subtracted from this varibale as the user answers questions. 
+
+// Questions problem - how to store information about each question (text of question, choices for answers, point value, correct answers)
+// Seperate these questions by category
+
+// Create a modal that displays when a user clicks a square that populates with information from that square's question. Change the background color of the modal to green if a user selects the correct answer (after submit). Change the background color to red if they select the incorrect answer (after submit). 
+
+// Create a function that adds the point value of the square clicked to the score if a user submits the correct answer.
+
+// Create a function that subtracts the point value of the square clicked from the score if the user submits an incorrect asnwer.
+
+// Create a reset button that resets the board back to the setup at the beginning of the game when clicked by the user.
+
+// Create an element that displays the name the user input at the bottom of the game board. - Player Name: "input"
